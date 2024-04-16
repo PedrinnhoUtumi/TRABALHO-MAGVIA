8d87497e44edc574d2b144b919e6d5a84d224adc
Pedro Felipe Onofre Utumi <130059527+PedrinnhoUtumi@users.noreply.github.com>
2024-04-16 01:53:23 -0300

    Trabalho

---
+++ testes/arquivo.cpp
@@ -0,0 +1,6 @@
+int main()
+{
+    // Vai guardar todo o código executado quando rodamos o programa
+    //  std::cout << "Mensagem" = print("Mensagem")
+    std::cout << "Bem vindos ao jogo da adivinhação!"
+}
