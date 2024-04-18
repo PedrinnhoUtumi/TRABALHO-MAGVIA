8997b9450d8fbe58d53625e0d830e89951a4679a
DanCeinoti <130059527+PedrinnhoUtumi@users.noreply.github.com>
2024-04-18 14:36:02 -0300

    Update arquivo.cpp

--- testes/arquivo.cpp
+++ testes/arquivo.cpp
@@ -1,6 +1,7 @@
-int main()
-{
+#include <iostream>
+
+int main(){
     // Vai guardar todo o código executado quando rodamos o programa
     //  std::cout << "Mensagem" = print("Mensagem")
-    std::cout << "Bem vindos ao jogo da adivinhação!"
+    std::cout << "Bem vindos ao jogo da adivinhação!" << std::endl;
 }
