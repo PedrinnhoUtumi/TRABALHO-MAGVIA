8e10d1d89fb8be50ba9a38d5fef7419f2485e873
DanCeinoti <130059527+PedrinnhoUtumi@users.noreply.github.com>
2024-04-22 16:03:29 -0300

    TRABALHO MAGVIA

--- inicio.py
+++ inicio.py
@@ -1,5 +1,5 @@
 from tkinter import *
-from PIL import Image, ImageDraw
+import time
 
 root = Tk()
 
@@ -21,23 +21,27 @@ class Application():
     def frames(self):
         self.frame1 = Frame(self.root, bd = 4, bg = "aqua", highlightbackground = "Blue", highlightthickness = 2)
         self.frame1.place(relx = 0.15, rely = 0.05, relwidth = 0.75, relheight = 0.3)
+        
         self.frame2 = Frame(self.root, bd = 4, bg = "aqua", highlightbackground = "Blue", highlightthickness = 2)
         self.frame2.place(relx = 0.05, rely = 0.6, relwidth = 0.9, relheight = 0.3)
         
     def botao(self):
         self.bt_game = Button(text = "Esportes")
         self.bt_game.place(relx = 0.05, rely = 0.05, relwidth = 0.1, relheight = 0.03)
+        
         self.bt_plus1 = Button(self.frame2, text = "+1")
         self.bt_plus1.place(relx = 0.05, rely = 0.05, relwidth = 0.05, relheight = 0.1)
         self.bt_minus1 = Button(self.frame2, text = "-1")
         self.bt_minus1.place(relx = 0.9, rely = 0.05, relwidth = 0.05, relheight = 0.1)
+        
         self.bt_plus2 = Button(self.frame2, text = "+2")
         self.bt_plus2.place(relx = 0.05, rely = 0.25, relwidth = 0.05, relheight = 0.1)
         self.bt_minus2 = Button(self.frame2, text = "-2")
         self.bt_minus2.place(relx = 0.9, rely = 0.25, relwidth = 0.05, relheight = 0.1)
+        
         self.bt_plus3 = Button(self.frame2, text = "+3")
         self.bt_plus3.place(relx = 0.05, rely = 0.45, relwidth = 0.05, relheight = 0.1)
         self.bt_minus3 = Button(self.frame2, text = "-3")
         self.bt_minus3.place(relx = 0.9, rely = 0.45, relwidth = 0.05, relheight = 0.1)
-        
+    
 Application()
