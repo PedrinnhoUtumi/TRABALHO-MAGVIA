112d04fad052823f232e1b44407d2b29cfe166ac
DanCeinoti <130059527+PedrinnhoUtumi@users.noreply.github.com>
2024-04-25 15:58:44 -0300

    TRABALHO MAGVIA

--- inicio.py
+++ inicio.py
@@ -38,7 +38,7 @@ class Interface():
         self.frameTime = Label(self.root, bg = "aqua", highlightbackground = "Blue", highlightthickness = 2, font = ("Arial", 48, "bold"), textvariable = self.placarTempo)
         self.frameTime.place(relx = 0.425, rely = 0.35, relwidth = 0.2, relheight = 0.2) #Tempo
         
-        self.frameVisitante = Label(self.root, bg = "aqua", highlightbackground = "Blue", highlightthickness = 2, font = ("Arial", 48, "bold"), textvariable = self.placarVisitante,  cursor = "hand1")
+        self.frameVisitante = Label(self.root, bg = "aqua", highlightbackground = "Blue", highlightthickness = 2, font = ("Arial", 48, "bold"), textvariable = self.placarVisitante, cursor = "hand1")
         self.frameVisitante.place(relx = 0.70, rely = 0.35, relwidth = 0.2, relheight = 0.2) #Placar time visitante
         self.frameVisitante.bind("<Button-1>", lambda event: self.plus(2))
         
@@ -46,43 +46,43 @@ class Interface():
         self.frame2.place(relx = 0.15, rely = 0.65, relwidth = 0.75, relheight = 0.2) #Botões variados
         
     def botao(self):
-        self.bt_game = Button(self.frame2, text = "Esportes")
+        self.bt_game = Button(self.frame2, text = "Esportes", cursor = "hand1")
         self.bt_game.place(relx = 0.42, rely = 0.05, relwidth = 0.15, relheight = 0.15)
         
-        self.bt_zero = Button(self.frame2, text = "Zerar", command = self.zero)
+        self.bt_zero = Button(self.frame2, text = "Zerar", command = self.zero, cursor = "hand1")
         self.bt_zero.place(relx = 0.42, rely = 0.25, relwidth = 0.15, relheight = 0.15)
         
-        self.bt_pause = Button(self.frame2, text = "Pausar", command = lambda: self.pause(1))
+        self.bt_pause = Button(self.frame2, text = "Pausar", command = lambda: self.pause(1), cursor = "hand1")
         self.bt_pause.place(relx = 0.42, rely = 0.45, relwidth = 0.15, relheight = 0.15)
         
-        self.bt_reniciar = Button(self.frame2, text = "Reiniciar", command = lambda: self.pause(2))
+        self.bt_reniciar = Button(self.frame2, text = "Reiniciar", command = lambda: self.pause(2), cursor = "hand1")
         self.bt_reniciar.place(relx = 0.42, rely = 0.65, relwidth = 0.15, relheight = 0.15)
         
-        self.bt_continue = Button(self.frame2, text = "Continuar")
+        self.bt_continue = Button(self.frame2, text = "Continuar", cursor = "hand1")
         self.bt_continue.place(relx = 0.61, rely = 0.05, relwidth = 0.15, relheight = 0.15)
         
-        self.bt_minus = Button(self.frame2, text = "-1", command = lambda: self.minus(1))
+        self.bt_minus = Button(self.frame2, text = "-1", command = lambda: self.minus(1), cursor = "hand1")
         self.bt_minus.place(relx = 0.05, rely = 0.05, relwidth = 0.15, relheight = 0.15)
         
-        self.plus1 = Button(self.frame2, text = "+1", command = lambda: self.plus(1))
+        self.plus1 = Button(self.frame2, text = "+1", command = lambda: self.plus(1), cursor = "hand1")
         self.plus1.place(relx = 0.05, rely = 0.25, relwidth = 0.15, relheight = 0.15)
         
-        self.bt_plus2 = Button(self.frame2, text = "+2", command = lambda: self.plus2(1))
+        self.bt_plus2 = Button(self.frame2, text = "+2", command = lambda: self.plus2(1), cursor = "hand1")
         self.bt_plus2.place(relx = 0.05, rely = 0.45, relwidth = 0.15, relheight = 0.15)
         
-        self.bt_plus3 = Button(self.frame2, text = "+3", command = lambda: self.plus3(1))
+        self.bt_plus3 = Button(self.frame2, text = "+3", command = lambda: self.plus3(1), cursor = "hand1")
         self.bt_plus3.place(relx = 0.05, rely = 0.65, relwidth = 0.15, relheight = 0.15)
         
-        self.bt_minus_visitante = Button(self.frame2, text = "-1", command = lambda: self.minus(2))
+        self.bt_minus_visitante = Button(self.frame2, text = "-1", command = lambda: self.minus(2), cursor = "hand1")
         self.bt_minus_visitante.place(relx = 0.8, rely = 0.05, relwidth = 0.15, relheight = 0.15)
         
-        self.plus_visitante = Button(self.frame2, text = "+1", command = lambda: self.plus(2))
+        self.plus_visitante = Button(self.frame2, text = "+1", command = lambda: self.plus(2), cursor = "hand1")
         self.plus_visitante.place(relx = 0.8, rely = 0.25, relwidth = 0.15, relheight = 0.15)
          
-        self.bt_plus2_visitante = Button(self.frame2, text = "+2", command = lambda: self.plus2(2))
+        self.bt_plus2_visitante = Button(self.frame2, text = "+2", command = lambda: self.plus2(2), cursor = "hand1")
         self.bt_plus2_visitante.place(relx = 0.8, rely = 0.45, relwidth = 0.15, relheight = 0.15)
         
-        self.bt_plus3_visitante = Button(self.frame2, text = "+3", command = lambda: self.plus3(2))
+        self.bt_plus3_visitante = Button(self.frame2, text = "+3", command = lambda: self.plus3(2), cursor = "hand1")
         self.bt_plus3_visitante.place(relx = 0.8, rely = 0.65, relwidth = 0.15, relheight = 0.15)
         
     def plus(self, team):
@@ -105,9 +105,11 @@ class Interface():
     
     def minus(self, team):
         if team == 1: 
-            self.placarLocal.set(self.placarLocal.get() - 1)
+            if self.validate(1):
+                self.placarLocal.set(self.placarLocal.get() - 1)
         elif team == 2:
-            self.placarVisitante.set(self.placarVisitante.get() - 1)
+            if self.validate(2):
+                self.placarVisitante.set(self.placarVisitante.get() - 1)
     
     def update(self):
         if self.contador:
@@ -133,7 +135,18 @@ class Interface():
         elif opcao == 2:
             self.contador = datetime.now()
             self.update()
-        
+    
+    def validate(self, opcao):
+        if opcao == 1:
+            if self.placarLocal.get() <= 0:
+                self.placarLocal.set(0)
+                return False
+        elif opcao == 2:
+            if self.placarVisitante.get() <= 0:
+                self.placarVisitante.set(0)
+                return False
+        return True
+            
 if __name__ == "__main__": 
     root = Tk()
     app = Interface(root)
