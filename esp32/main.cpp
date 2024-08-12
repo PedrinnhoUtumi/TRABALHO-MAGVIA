ff6b01860dc137b7a5aa177327a4acac9349a9f8
DanCeinoti <130059527+PedrinnhoUtumi@users.noreply.github.com>
2024-08-12 15:42:09 -0300

    Update main.cpp

--- esp32/main.cpp
+++ esp32/main.cpp
@@ -2,84 +2,95 @@
 #include <LiquidCrystal.h>
 #include "main.h"
 
-#define LedPlate 17 
-#define potentiometer 5 
-#define Lpwm 1 
-#define Rpwm 2 
-#define enableL 16 
-#define enableR 39 
-#define tictac 4 
+#define LedPlate 17
+#define potentiometer 5
+#define Lpwm 1
+#define Rpwm 2
+#define enableL 16
+#define enableR 39
+#define tictac 4
+#define pedalInput 7
+#define pedalOutput 15
+#define limitSwitch 40
 
-#define RS 3 
-#define E 46 
-#define D4 9 
-#define D5 35 
-#define D6 45 
-#define D7 37 
+#define RS 3
+#define E 46
+#define D4 9
+#define D5 35
+#define D6 45
+#define D7 37
 
 LiquidCrystal lcd(RS, E, D4, D5, D6, D7);
 
-//variaveis de configuração PWM
-const int channel = 1; //uint8_t
-const int freq = 1000; //uint32_t
-const int resolution = 12; //uint8_t
+// variaveis de configuração PWM
+const int channel = 1;      // uint8_t
+const int frequency = 1000; // uint32_t
+const int resolution = 12;  // uint8_t
 
 int direction;
-uint8_t voltas = 0;
-int pot;
-Tipo_MediaMovel mediaPot;
+uint16_t turns = 0;
+int potentiometerValue;
+Tipo_MediaMovel mediaPotentiometer;
 
-
-void start(){
-  
-}
-
-int updateDirection(){
+int updateDirection()
+{
   pinMode(tictac, INPUT_PULLDOWN);
   int button = digitalRead(tictac);
-  if (button == LOW){
+  if (button == LOW)
+  {
     direction = 1;
-    Serial.println("BOTAO PRESSIONADO");
-  } else {
+  }
+  else
+  {
     direction = 0;
-    Serial.println("BOTAO DESPRESSIONADO");
   }
   return direction;
 }
 
-void updateScreen(){
+void updateScreen()
+{
   lcd.clear();
   lcd.print("vel:");
 
-  lcd.print(pot * 100 / 4095);
+  lcd.print(potentiometerValue * 100 / 4095);
   lcd.print("%");
-  
+
   lcd.setCursor(9, 0);
-  if (!direction){
+  if (!direction)
+  {
     lcd.print("dir:AH");
-  } else { 
+  }
+  else
+  {
     lcd.print("dir:H");
   }
 
   lcd.setCursor(0, 1);
   lcd.print("Voltas:");
-  lcd.print(direction);
+  int limitSwitchValue = digitalRead(limitSwitch);
+  if (limitSwitchValue == 1.)
+  {
+    turns++;
+  }
+  lcd.print(turns);
 }
 
 uint16_t CalculaMediaMovel(Tipo_MediaMovel *pMediaMovel, uint16_t NovaAmostra)
 {
-	pMediaMovel->Soma += NovaAmostra - pMediaMovel->Fila[pMediaMovel->IndexFila];
-	pMediaMovel->Fila[pMediaMovel->IndexFila] = NovaAmostra;
-	pMediaMovel->IndexFila++;
-	if(pMediaMovel->IndexFila >= Amostras)
-		pMediaMovel->IndexFila = 0;
+  pMediaMovel->Soma += NovaAmostra - pMediaMovel->Fila[pMediaMovel->IndexFila];
+  pMediaMovel->Fila[pMediaMovel->IndexFila] = NovaAmostra;
+  pMediaMovel->IndexFila++;
+  if (pMediaMovel->IndexFila >= Amostras)
+    pMediaMovel->IndexFila = 0;
 
-	return pMediaMovel->Soma/Amostras;
-} 
+  return pMediaMovel->Soma / Amostras;
+}
 
-void rotation() {
-  if (direction == 0){
-    ledcSetup(channel, freq, resolution);  
+void rotation()
+{
+  if (direction == 0)
+  {
+    ledcSetup(channel, frequency, resolution);
     pinMode(enableL, OUTPUT);
     pinMode(enableR, OUTPUT);
     pinMode(Lpwm, OUTPUT);
@@ -89,8 +100,10 @@ void rotation() {
     digitalWrite(Rpwm, LOW);
     digitalWrite(Lpwm, HIGH);
     ledcAttachPin(Lpwm, channel);
-  } else {
-    ledcSetup(channel, freq, resolution);  
+  }
+  else
+  {
+    ledcSetup(channel, frequency, resolution);
     pinMode(enableL, OUTPUT);
     pinMode(enableR, OUTPUT);
     pinMode(Lpwm, OUTPUT);
@@ -100,33 +113,45 @@ void rotation() {
     digitalWrite(Lpwm, LOW);
     digitalWrite(Rpwm, HIGH);
     ledcAttachPin(Rpwm, channel);
-  } 
+  }
 }
 
-void setup(){
+void display()
+{
+  ledcWrite(channel, potentiometerValue);
+}
+
+void setup()
+{
   Serial.begin(115200);
-  
-  
-  
-  lcd.begin(16,2);
+  lcd.begin(16, 2);
 }
- 
-void loop(){
-  start();
-  
 
-
-
-  pot = CalculaMediaMovel(&mediaPot, analogRead(potentiometer));
-
-  ledcWrite(channel, pot);
-  Serial.println(pot);
-
-  rotation();
-
+void loop()
+{
+  potentiometerValue = CalculaMediaMovel(&mediaPotentiometer, analogRead(potentiometer));
+  display();
+  pinMode(7, INPUT_PULLDOWN);
+  pinMode(15, OUTPUT);
+  digitalWrite(15, HIGH);
+  int pedalValue = digitalRead(pedalInput);
   updateDirection();
   updateScreen();
-  voltas++;
+  rotation();
+  if (pedalValue == 1)
+  {
+    Serial.println(potentiometerValue);
 
-  delay(20);
+    // display();
+  }
+  else
+  {
+    pinMode(7, INPUT_PULLDOWN);
+    pinMode(15, OUTPUT);
+    digitalWrite(enableL, LOW);
+    digitalWrite(enableR, LOW);
+    digitalWrite(Lpwm, LOW);
+    digitalWrite(Rpwm, LOW);
+  }
+  delay(15);
 }
