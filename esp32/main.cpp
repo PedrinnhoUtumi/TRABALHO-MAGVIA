1b6b08dffc9e05c2c296441dbdf5f9e2aa0aaee9
DanCeinoti <130059527+PedrinnhoUtumi@users.noreply.github.com>
2024-08-12 13:49:45 -0300

    TR4ABALHO MAGVIA

--- esp32/main.cpp
+++ esp32/main.cpp
@@ -1,11 +1,14 @@
 #include <Arduino.h>
 #include <LiquidCrystal.h>
+#include "main.h"
 
 #define LedPlate 17 
 #define potentiometer 5 
 #define Lpwm 1 
 #define Rpwm 2 
-#define enable 16 
+#define enableL 16 
+#define enableR 39 
+#define tictac 4 
 
 #define RS 3 
 #define E 46 
@@ -21,45 +24,109 @@ const int channel = 1; //uint8_t
 const int freq = 1000; //uint32_t
 const int resolution = 12; //uint8_t
 
-int direction = 1;
+int direction;
 uint8_t voltas = 0;
 int pot;
+Tipo_MediaMovel mediaPot;
 
-void update_screen(){
+
+void start(){
+  
+}
+
+int updateDirection(){
+  pinMode(tictac, INPUT_PULLDOWN);
+  int button = digitalRead(tictac);
+  if (button == LOW){
+    direction = 1;
+    Serial.println("BOTAO PRESSIONADO");
+  } else {
+    direction = 0;
+    Serial.println("BOTAO DESPRESSIONADO");
+  }
+  return direction;
+}
+
+void updateScreen(){
   lcd.clear();
-  lcd.print("v: ");
-  lcd.print(pot);
-  lcd.setCursor(8, 0);
+  lcd.print("vel:");
+
+  lcd.print(pot * 100 / 4095);
+  lcd.print("%");
+  
+  lcd.setCursor(9, 0);
   if (!direction){
-    lcd.print("S: AH");
+    lcd.print("dir:AH");
   } else { 
-    lcd.print("S: H");
+    lcd.print("dir:H");
   }
 
   lcd.setCursor(0, 1);
-  lcd.print("Voltas: ");
-  lcd.print(voltas);
+  lcd.print("Voltas:");
+  lcd.print(direction);
+}
+
+uint16_t CalculaMediaMovel(Tipo_MediaMovel *pMediaMovel, uint16_t NovaAmostra)
+{
+	pMediaMovel->Soma += NovaAmostra - pMediaMovel->Fila[pMediaMovel->IndexFila];
+	pMediaMovel->Fila[pMediaMovel->IndexFila] = NovaAmostra;
+	pMediaMovel->IndexFila++;
+	if(pMediaMovel->IndexFila >= Amostras)
+		pMediaMovel->IndexFila = 0;
+
+	return pMediaMovel->Soma/Amostras;
+} 
+
+void rotation() {
+  if (direction == 0){
+    ledcSetup(channel, freq, resolution);  
+    pinMode(enableL, OUTPUT);
+    pinMode(enableR, OUTPUT);
+    pinMode(Lpwm, OUTPUT);
+    pinMode(Rpwm, OUTPUT);
+    digitalWrite(enableR, LOW);
+    digitalWrite(enableL, HIGH);
+    digitalWrite(Rpwm, LOW);
+    digitalWrite(Lpwm, HIGH);
+    ledcAttachPin(Lpwm, channel);
+  } else {
+    ledcSetup(channel, freq, resolution);  
+    pinMode(enableL, OUTPUT);
+    pinMode(enableR, OUTPUT);
+    pinMode(Lpwm, OUTPUT);
+    pinMode(Rpwm, OUTPUT);
+    digitalWrite(enableL, LOW);
+    digitalWrite(enableR, HIGH);
+    digitalWrite(Lpwm, LOW);
+    digitalWrite(Rpwm, HIGH);
+    ledcAttachPin(Rpwm, channel);
+  } 
 }
 
 void setup(){
   Serial.begin(115200);
-  pinMode(enable, OUTPUT);
-  pinMode(Rpwm, OUTPUT);
-  digitalWrite(enable, HIGH);
-  digitalWrite(Rpwm, LOW);
-  ledcSetup(channel, freq, resolution);  
-  ledcAttachPin(Lpwm, channel);
+  
+  
   
   lcd.begin(16,2);
 }
  
 void loop(){
-  pot = analogRead(potentiometer);
+  start();
+  
+
+
+
+  pot = CalculaMediaMovel(&mediaPot, analogRead(potentiometer));
+
   ledcWrite(channel, pot);
   Serial.println(pot);
 
-  update_screen();
+  rotation();
+
+  updateDirection();
+  updateScreen();
   voltas++;
-  
-  delay(100);
+
+  delay(20);
 }
--- python teste/teste.py
+++ python teste/teste.py
@@ -1,18 +1,7 @@
-import tkinter as tk
-from tkinter import Tk
+velocidade = int(input("valor: "))
+tempo = int(input("valor: "))
 
-def redesenhar(event):
-    root.update()
-    
-if __name__ == "__main__":
-    root = Tk()
-    root.title("Redesenho Automático da Página")
-
-    # Aqui você pode adicionar os widgets da sua interface gráfica
-    label = tk.Label(root, text="Clique em qualquer lugar para redesenhar a página", font=("Helvetica", 18))
-    label.pack(padx=20, pady=20)
-
-    # Vincular todos os eventos de clique para chamar a função redesenhar
-    root.bind_all("<Button-1>", redesenhar)
-
-    root.mainloop()
+if velocidade >= -100 and velocidade <= 100: 
+    if tempo >= 0 and tempo <= 200:
+        resultado = velocidade * (2 * tempo)
+        print(f"Resultado: {resultado}")
